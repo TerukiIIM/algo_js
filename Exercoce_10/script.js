32e444b7c909baf56c5bdeecd9d4481d2797ddfd
TerukiIIM <147390861+TerukiIIM@users.noreply.github.com>
2023-10-13 09:59:21 +0200

    script.js

--- Exercoce_10/script.js
+++ Exercoce_10/script.js
@@ -5,8 +5,8 @@ function countCharacter(value) {
 }
 
 users.forEach(user => {
-    if (countcharacter(users) < 5) {
-        console.log('cest un prénom long de plus de 5 lettres.')
+    if (countCharacter(user) < 5) {
+        console.log("c'est un prénom long de plus de 5 lettres.")
     } else {
         console.log("ce n'est pas du tout un prénom long.")
     }
